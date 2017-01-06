8f795742ade25bec2b2523395ec7ce67c87cfd15
bernadettekelly <bernadettemkelly54@gmail.com>
2017-01-06 11:00:28 -0500

    changes

--- models.js
+++ models.js
@@ -3,9 +3,27 @@ const mongoose = require('mongoose');
 const blogPostsSchema = mongoose.Schema({
 
 	title: {type: String, required: true},
-	author: {type: String, required: true},
+	author: {
+		firstName: String,
+		lastname: String
+	},
 	content: {type: String, required: true},
 	created: {date: Date, required: true}
 });
  
-		
+blogPostsSchema.virtual('title').get(function() {
+	return `${this.title}`.trim();
+});
+
+blogPostsSchema.methods.apiRepr = function() {
+	return {
+		id: this._id,
+		title: this.title,
+		author: this.author,
+		content: this.content,
+		created: this.created
+    };
+}
+
+const blogPosts = mongoose.model('blogPosts', blogPostsSchema);
+module.exports = {blogPosts};
